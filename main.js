0b46dc85e56ae3ab9f4750b3ae42ee0110931ea9
Ken Coppola <mwave317@gmail.com>
2017-08-10 16:37:19 -0400

    Uploading the new files

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 let section = document.querySelector("section");
+  let input = document.querySelector("input");
+let textarea;
+
 
 function convertJson(response) {
   return response.json();
 }
+function timeout(answer) {
+window.setTimeout(function() {
+  if (textarea.value !== answer) {
+    console.log("Wrong Answer");
+  } else
+    console.log("Congradulations");
 
-let question = "";
+}, 1000);
+};
 
 function playgame(response) {
-    console.log(response[0].answer);
+
       let form =`
         <h1>Let's play Jeporday!</h1>
         <h2>${response[0].question} --- Point Value ${response[0].value}</h2>
         <label for="answer">Answer: </label>
-        <textarea rows="5" cols="51" name="answer" id="answer" placeholder="Enter your answer"></textarea>
-        <input type="submit" value="submit"></button>
+        <textarea rows="3" cols="51" name="answer" id="answer" placeholder="Enter your answer"></textarea>
+        <input type="button" value="submit" onclick="timeout('${response[0].answer}')"></button>
       `;
 
 section.innerHTML = form;
-
-let input = document.querySelector("#answer");
-console.log(response[0].id);
-if (response[0].answer !== input){
-  console.log("Hello from the if statement");
-
-  section.innerHTML = form;
-}
-
+//}
+  console.log(response[0].answer);
+  textarea = document.querySelector("textarea");
 }
 
 function wrong () {
