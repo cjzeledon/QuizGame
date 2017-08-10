d5062d3223062894632badd15e1c4af1e8d6294b
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-10 13:16:53 -0400

    Beginning Project

---
+++ main.js
@@ -0,0 +1,39 @@
+let section = document.querySelector("section");
+
+function convertJson(response) {
+  return response.json();
+}
+
+let question = "";
+
+function playgame(response) {
+    console.log(response[0].answer);
+      let form =`
+        <h1>Let's play Jeporday!</h1>
+        <h2>${response[0].question} --- Point Value ${response[0].value}</h2>
+        <label for="answer">Answer: </label>
+        <input type="text" name="answer" id="answer" placeholder="Enter your answer">
+        <input type="submit" value="submit"></button>
+      `;
+
+section.innerHTML = form;
+
+let input = document.querySelector("#answer");
+console.log(input.value);
+if (response[0].answer !== input){
+  console.log("Hello from the if statement");
+
+  section.innerHTML = form;
+}
+
+}
+
+function wrong () {
+  console.log("Nice Try");
+}
+
+
+fetch("http://jservice.io/api/random")
+  .then(convertJson)
+  .then(playgame)
+  .catch(wrong);
